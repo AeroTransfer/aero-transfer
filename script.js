a89389575e8a2c9f0b3bfbd76f35217806b6df96
Sanjot Brar <sanjotbrar2003@gmail.com>
2026-08-12 17:02:36 +0530

    Fix javaScript syntax error

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const nav = document.querySelector(".header nav");
 
     if (menuToggle && nav) {
-        menuToggle.onclick = function () {
+        menuToggle.addEventListener("click", function () {
             nav.classList.toggle("active");
-        };
+        });
     }
 
 });
